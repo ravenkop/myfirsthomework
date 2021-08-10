7772bae08d0b35f87249d789e076151c4d5439a7
Raven Kopru <ravenkopru@gmail.com>
2021-08-09 21:42:23 -0500

    search api
    added search api with JSON file and made the Search bar functional

--- socialmedia/src/components/Navigation.js
+++ socialmedia/src/components/Navigation.js
@@ -3,6 +3,8 @@ import Menu from "./Menu";
 import logo from "../images/instagramLogo2.PNG";
 import SearchBar from "./SearchBar";
 // import searchIcon from "../images/searchIcon.png";
+import JSONDATA from './Mock_Data.json';
+import {useState} from 'react';
 
 
 function handleSubmit(e) {
@@ -12,14 +14,24 @@ function handleSubmit(e) {
 
 
 function Navigation() {
+  const [searchTerm, setSearchTerm] = useState("")
   return (
     <div className="navigation">
       <div className="container">
         <img className="logo" src={logo} alt="instagram logo" />
         <div className="search">
           {/* <img className="searchIcon" src={searchIcon} alt="search icon" /> */}
-          <input className="boxed" type="text" />
-          <button className="searchText" onClick={handleSubmit}>Search</button>
+          <input className="boxed" type="text" placeholder="Search" onChange={event => {setSearchTerm(event.target.value)}} />
+          {JSONDATA.filter((val)=> {
+            if(searchTerm == "") {
+
+            } else if (val.username.toLowerCase().includes(searchTerm.toLowerCase())) {
+              return val
+            }
+          }).map((val,key)=> {
+            return <div className="user" key={key}>{val.username}</div>
+          })}
+          {/* <button className="searchText" onClick={handleSubmit}>Search</button> */}
         </div>
         <Menu />
       </div>
