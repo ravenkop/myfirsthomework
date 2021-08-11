3eed723b6b7e14e9a66b81433b0ca236f84b5b7e
Raven Kopru <ravenkopru@gmail.com>
2021-08-11 01:05:25 -0500

    Update App.js

--- peri/src/App.js
+++ peri/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React test dfggf
+          Learn React
         </a>
       </header>
     </div>
