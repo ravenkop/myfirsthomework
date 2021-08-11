bc5c8184ec5674c0c82ce0d765caf815a8f1cd33
Raven Kopru <ravenkopru@gmail.com>
2021-08-11 00:09:52 -0500

    another error
    same errors now gitbash is not working for me

---
+++ socialmedia/socialmedia/src/components/App.js
@@ -0,0 +1,22 @@
+import "../styles/App.scss";
+import Navigation from "./Navigation";
+import Cards from "./Cards";
+import Sidebar from "./Sidebar";
+import SearchPage from './SearchPage';
+
+function App() {
+  return (
+    <div className="App">
+      <Navigation />
+      <main>
+        <div className="container">
+          <Cards />
+          <Sidebar />
+        </div>
+
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ socialmedia/socialmedia/src/components/Navigation.js
@@ -0,0 +1,42 @@
+import "../styles/navigation.scss";
+import Menu from "./Menu";
+import logo from "../images/instagramLogo2.PNG";
+import SearchBar from "./SearchBar";
+// import searchIcon from "../images/searchIcon.png";
+import JSONDATA from './Mock_Data.json';
+import {useState} from 'react';
+
+
+function handleSubmit(e) {
+  e.preventDefault();
+}
+
+
+
+function Navigation() {
+  const [searchTerm, setSearchTerm] = useState("")
+  return (
+    <div className="navigation">
+      <div className="container">
+        <img className="logo" src={logo} alt="instagram logo" />
+        <div className="search">
+          {/* <img className="searchIcon" src={searchIcon} alt="search icon" /> */}
+          <input className="boxed" type="text" placeholder="Search" onChange={event => {setSearchTerm(event.target.value)}} />
+          {JSONDATA.filter((val)=> {
+            if(searchTerm == "") {
+
+            } else if (val.username.toLowerCase().includes(searchTerm.toLowerCase())) {
+              return val
+            }
+          }).map((val,key)=> {
+            return <div className="user" key={key}>{val.username}</div>
+          })}
+          {/* <button className="searchText" onClick={handleSubmit}>Search</button> */}
+        </div>
+        <Menu />
+      </div>
+    </div>
+  );
+}
+
+export default Navigation;
