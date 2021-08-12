4d3b14b0e2819dc8bfb121cd87c8080a4263e9b0
Raven Kopru <ravenkopru@gmail.com>
2021-08-12 01:18:27 -0500

    Added components
    added products, product, images, and homescreen components. Also created my inventory file to refer back to and fixed some styling.

--- peri/src/App.js
+++ peri/src/App.js
@@ -2,17 +2,19 @@ import React from 'react';
 import { Container } from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import HomeScreen from './screens/HomeScreen'
+
 const App = () => {
   return (
-    <div className="App">
+    <>
       <Header />
       <main className='py-3'>
         <Container>
-      <h1>Welcome To Peri Moda</h1>
+          <HomeScreen />
       </Container>
       </main>
       <Footer />
-    </div>
+    </>
   );
 }
 
