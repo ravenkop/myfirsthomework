33f74decf4aec053e0bfbe5ec766c5538ba86067
Raven Kopru <ravenkopru@gmail.com>
2021-08-11 20:02:43 -0500

    Base page layout/theme
    imported some css and themes that I wanted and put down my brand and copyright. Also made sure I liked the spacing and the colors/style.

--- peri/src/App.js
+++ peri/src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import { Container } from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
 const App = () => {
   return (
     <div className="App">
       <Header />
-      <main>
+      <main className='py-3'>
+        <Container>
       <h1>Welcome To Peri Moda</h1>
+      </Container>
       </main>
       <Footer />
     </div>
