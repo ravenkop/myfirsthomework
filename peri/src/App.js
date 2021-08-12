6f159756087a1525926a4ff09dc739e432adc7ba
Raven Kopru <ravenkopru@gmail.com>
2021-08-11 19:32:29 -0500

    Created new files
    Created header and footer and then imported them to app.js

--- peri/src/App.js
+++ peri/src/App.js
@@ -1,11 +1,14 @@
-
 import React from 'react';
-
-
+import Header from './components/Header'
+import Footer from './components/Footer'
 const App = () => {
   return (
     <div className="App">
+      <Header />
+      <main>
       <h1>Welcome To Peri Moda</h1>
+      </main>
+      <Footer />
     </div>
   );
 }
