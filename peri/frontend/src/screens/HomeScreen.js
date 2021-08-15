a2eebffb361cdba7f525d0bd6970ccaabca8bd74
Raven Kopru <ravenkopru@gmail.com>
2021-08-15 17:23:29 -0500

    cart set up
    I set up the boiler for my cart and imported the necessary files and const functions needed to update quantities and redirect the site as needed

--- peri/frontend/src/screens/HomeScreen.js
+++ peri/frontend/src/screens/HomeScreen.js
@@ -10,7 +10,7 @@ import {listProducts} from '../actions/productActions'
 const HomeScreen = () => {
   const dispatch = useDispatch()
 
-  const productList = useSelector(state => state.productList)
+  const productList = useSelector((state) => state.productList)
   const{ loading, error, products } = productList
 
   useEffect(() => {
@@ -22,7 +22,7 @@ const HomeScreen = () => {
   return (
     <>
       <h1>Women's Clothing and Accesories</h1>
-      {loading ? <Loader /> : error ? (<Message variant='danger'>(error)</Message>) : <Row>
+      {loading ? (<Loader />) : error ? (<Message variant='danger'>(error)</Message>) : (<Row>
             {products.map((product) => (
               <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
                 <Product product={product} />
@@ -30,7 +30,7 @@ const HomeScreen = () => {
             ))}
           </Row>
 
-          }
+      )}
 
     </>
   )
