60c66456122a0a493d8edad27562f0443e486c4c
Raven Kopru <ravenkopru@gmail.com>
2021-08-16 05:01:10 -0500

    backend js
    added js files to the backend so that orders and inventory can route to the right places

--- peri/backend/middleware/errorMiddleware.js
+++ peri/backend/middleware/errorMiddleware.js
@@ -10,7 +10,7 @@ const errorHandler = (err, req, res, next) => {
   res.statusCode(statusCode)
   res.json({
     message: err.message,
-    stack: process.env.NODE_ENV === 'production' ? null : err.stack
+    stack: process.env.NODE_ENV === 'production' ? null : err.stack,
   })
 }
 
