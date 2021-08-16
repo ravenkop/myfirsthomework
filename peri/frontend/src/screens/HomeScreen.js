0f0c295eef656d47b7553e3d569fcc5c16427def
Raven Kopru <ravenkopru@gmail.com>
2021-08-16 12:47:27 -0500

    errors
    errors fixed

--- peri/frontend/src/screens/HomeScreen.js
+++ peri/frontend/src/screens/HomeScreen.js
@@ -7,14 +7,16 @@ import Loader from '../components/Loader'
 import {listProducts} from '../actions/productActions'
 
 
-const HomeScreen = () => {
+const HomeScreen = ({match}) => {
+
+  const keyword = match.params.keyword
   const dispatch = useDispatch()
 
   const productList = useSelector((state) => state.productList)
   const{ loading, error, products } = productList
 
   useEffect(() => {
-    dispatch(listProducts())
+    dispatch(listProducts(keyword))
   }, [dispatch])
 
 
